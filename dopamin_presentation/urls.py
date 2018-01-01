fe5f96cb6738c057722973d2ea76d6c233393362
László Hegedűs <heglas11@gmail.com>
2018-01-01 12:25:21 +0100

    Urls.

--- dopamin_presentation/urls.py
+++ dopamin_presentation/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='dopamin_presentation/index.html'), name='index'),
+    url(r'^presentation/$', TemplateView.as_view(template_name='dopamin_presentation/index.html'), name='index'),
+    url(r'^src/my-app$', TemplateView.as_view(template_name='dopamin_presentation/my-app.html'), name='application'),
 ]
