fba6b72a04940c3efe3960ae7c2141edf3a37eb0
László Hegedűs <heglas11@gmail.com>
2018-01-01 14:04:22 +0100

    Stg

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='dopamin-presentation',
-    version='0.0.12',
+    version='0.0.13',
     packages=find_packages(),
     include_package_data=True,
     license='Apache 2.0 License',  # example license
